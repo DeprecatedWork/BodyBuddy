37f7a2b990d5b7c8ad1053a33c58dd1fac204db8
Jess <code.jessw@gmail.com>
2015-10-25 09:35:17 +0000

    Hack Man Shizz
    Body Buddy website

---
+++ BodyBuddy/App_Start/AuthConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Web.WebPages.OAuth;
+using BodyBuddy.Models;
+
+namespace BodyBuddy
+{
+    public static class AuthConfig
+    {
+        public static void RegisterAuth()
+        {
+          
+
+            // To let users of this site log in using their accounts from other sites such as Microsoft, Facebook, and Twitter,
+            // you must update this site. For more information visit http://go.microsoft.com/fwlink/?LinkID=252166
+
+            //OAuthWebSecurity.RegisterMicrosoftClient(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            //OAuthWebSecurity.RegisterTwitterClient(
+            //    consumerKey: "",
+            //    consumerSecret: "");
+
+            OAuthWebSecurity.RegisterFacebookClient(
+                appId: "1507333236231737",
+                appSecret: "3098de841bfd087871a86382378978d0");
+
+            //OAuthWebSecurity.RegisterGoogleClient();
+        }
+    }
+}
